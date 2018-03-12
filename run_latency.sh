bf63005b876304771e9837ad3f53e073f9330104
Palm <palmchou@live.com>
2018-03-11 23:01:06 -0700

    fix bug

--- run_latency.sh
+++ run_latency.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-REPEAT=100
 CMD="python test.py data/test_x_256.csv 2"
-python time_monitor/time_monitor.py -r $REPEAT -o ${RESULT_DIR}/latency_r_${REPEAT}.csv $CMD
+python time_monitor/time_monitor.py -r $LATENCY_REPEAT -o ${RESULT_DIR}/latency_r_${REPEAT}.csv $CMD
 
